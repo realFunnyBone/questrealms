56bb8169d1d08e519c6168ff95cbca7b470aea61
realFunnyBone <31138897+realFunnyBone@users.noreply.github.com>
2018-06-25 11:41:07 -0400

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo off
+title Minecraft Server Console [Bukkit 1.12.2] @xSplayd
+color 0e
+cls
+java -Xmx1024M -jar craftbukkit.jar -o true
+echo.
+echo ----------------------------------------------------------------------------------------
+echo                         // PRESS ANY KEY TO END CONSOLE \\
+echo ----------------------------------------------------------------------------------------
+echo            PLEASE READ THROUGH THE READ ME FILE IF YOU HAVE NOT YET!
+echo ----------------------------------------------------------------------------------------
+echo          \\ Subscribe to the channel and follow on Twitter: @xSplayd //
+echo ----------------------------------------------------------------------------------------
+PAUSE>nul
+exit()
